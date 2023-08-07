59f131ca7ff77f9097e01a844b0a83d1a2728105
gauriiii13 <101903173+gauriiii13@users.noreply.github.com>
2023-08-07 12:31:28 +0530

    Update public_dps.py

--- public_dps.py
+++ public_dps.py
@@ -15,40 +15,40 @@ diabetes_model=pickle.load(open("diabetes_model.sav",'rb'))
 st.title('Diabetes Prediction Using ML')
 col1,col2,col3=st.columns(3)
 with col1:
-  Pregnancies=st.text_input('Number of Pregnancies')
-  if not Pregnancies:
-        st.error("Please enter number of pregnancies")  
+  Pregnancies=st.text_input('Number of Pregnancies')  
 with col2: 
   Glucose=st.text_input('Glucose Level')
-  if not Glucose:
-        st.error("Please enter Glucose Level")  
 with col3:
-  BloodPressure=st.text_input('Blood Pressure Value')
-  if not BloodPressure:
-        st.error("Please Blood Pressure Value")  
+  BloodPressure=st.text_input('Blood Pressure Value')  
 with col1:
   SkinThickness=st.text_input('Skin Thickness Value')
-  if not SkinThickness:
-        st.error("Please enter Skin Thickness Value")  
 with col2:
-  Insulin=st.text_input('Insulin Value')
-  if not Insulin:
-        st.error("Please enter Insulin Value")   
+  Insulin=st.text_input('Insulin Value')  
 with col3:
-  BMI=st.text_input('BMI Value')
-  if not BMI:
-        st.error("Please enter BMI Value")   
+  BMI=st.text_input('BMI Value')   
 with col1:
   DiabetesPedigreeFunction=st.text_input('Diabetes Pedigree Function Value')
-  if not DiabetesPedigreeFunction:
-        st.error("Please Diabetes Pedigree Function Value")   
 with col2:
-  Age=st.text_input('Age of the person')
-  if not Age:
-        st.error("Please enter age")   
+  Age=st.text_input('Age of the person')  
 #code for prediction
 diab_diagnosis=''
 if st.button('Diabetes Test Result'):
+  if not Pregnancies:
+     st.error("Please enter number of pregnancies")  
+  if not Glucose:
+     st.error("Please enter Glucose Level") 
+  if not BloodPressure:
+     st.error("Please Blood Pressure Value")    
+  if not SkinThickness:
+     st.error("Please enter Skin Thickness Value")  
+  if not Insulin:
+     st.error("Please enter Insulin Value") 
+  if not BMI:
+     st.error("Please enter BMI Value")    
+  if not DiabetesPedigreeFunction:
+     st.error("Please Diabetes Pedigree Function Value")      
+  if not Age:
+     st.error("Please enter age")         
   diab_prediction=diabetes_model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
   if diab_prediction[0]==1:
      diab_diagnosis='The person is diabetic'
